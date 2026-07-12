8cdbf7acd5e6224b442f04b57d43169df1114f34
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "application_load_balancer_frontends" {
-  description = "All application_load_balancer_frontend resources"
-  value       = azurerm_application_load_balancer_frontend.application_load_balancer_frontends
-}
 output "application_load_balancer_frontends_application_load_balancer_id" {
-  description = "List of application_load_balancer_id values across all application_load_balancer_frontends"
-  value       = [for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : v.application_load_balancer_id]
+  description = "Map of application_load_balancer_id values across all application_load_balancer_frontends, keyed the same as var.application_load_balancer_frontends"
+  value       = { for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : k => v.application_load_balancer_id }
 }
 output "application_load_balancer_frontends_fully_qualified_domain_name" {
-  description = "List of fully_qualified_domain_name values across all application_load_balancer_frontends"
-  value       = [for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : v.fully_qualified_domain_name]
+  description = "Map of fully_qualified_domain_name values across all application_load_balancer_frontends, keyed the same as var.application_load_balancer_frontends"
+  value       = { for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : k => v.fully_qualified_domain_name }
 }
 output "application_load_balancer_frontends_name" {
-  description = "List of name values across all application_load_balancer_frontends"
-  value       = [for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : v.name]
+  description = "Map of name values across all application_load_balancer_frontends, keyed the same as var.application_load_balancer_frontends"
+  value       = { for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : k => v.name }
 }
 output "application_load_balancer_frontends_tags" {
-  description = "List of tags values across all application_load_balancer_frontends"
-  value       = [for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : v.tags]
+  description = "Map of tags values across all application_load_balancer_frontends, keyed the same as var.application_load_balancer_frontends"
+  value       = { for k, v in azurerm_application_load_balancer_frontend.application_load_balancer_frontends : k => v.tags }
 }
 
